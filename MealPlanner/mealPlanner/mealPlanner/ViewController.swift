5e4303ab223758e3d88175c000161970c957cda2
mrncmoose <mrncmoose@gmail.com>
2017-02-25 18:14:49 -0600

    working UI that pulls meal data from salesforce

---
+++ MealPlanner/mealPlanner/mealPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mealPlanner
+//
+//  Created by moose on 2/24/17.
+//  Copyright © 2017 Fred Dunaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
