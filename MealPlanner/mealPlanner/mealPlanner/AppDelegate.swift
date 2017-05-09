ee8adb14b6d43a2bbf0c05df0039209c2afdfc82
mrncmoose <mrncmoose@gmail.com>
2017-05-09 08:55:09 -0400

    Adding Native Apple iOS app

--- MealPlanner/mealPlanner/mealPlanner/AppDelegate.swift
+++ MealPlanner/mealPlanner/mealPlanner/AppDelegate.swift
@@ -7,18 +7,60 @@
 //
 
 import UIKit
+import SalesforceSDKCore
+import os.log
+
+// Fill these in when creating a new Connected Application on Force.com
+let RemoteAccessConsumerKey = "3MVG9uudbyLbNPZMEhXZt1Nfa7alq1Y0qk8KAA4f4zjEOJZCOq_M__H1kmjYaztKyS3niZ0mBdxxwzpAGIbKS";
+let OAuthRedirectURI        = "mysampleapp://auth/success";
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    /// Salesforce Connected App properties (replace with your own…) /* 2 */
+    let consumerKey = RemoteAccessConsumerKey
+    let redirectURL = URL(string: OAuthRedirectURI)!
+    
+
+    override
+    init()
+    {
+        super.init()
+        SFLogger.shared().logLevel = .debug
+        
+        SalesforceSDKManager.shared().connectedAppId = RemoteAccessConsumerKey
+        SalesforceSDKManager.shared().connectedAppCallbackUri = OAuthRedirectURI
+        SalesforceSDKManager.shared().authScopes = ["web", "api"];
+        SalesforceSDKManager.shared().postLaunchAction = {
+            [unowned self] (launchActionList: SFSDKLaunchAction) in
+            let launchActionString = SalesforceSDKManager.launchActionsStringRepresentation(launchActionList)
+            self.log(.info, msg:"Post-launch: launch actions taken: \(launchActionString)");
+//            self.setupRootViewController();
+        }
+        SalesforceSDKManager.shared().launchErrorAction = {
+            [unowned self] (error: Error, launchActionList: SFSDKLaunchAction) in
+            self.log(.error, msg:"Error during SDK launch: \(error.localizedDescription)")
+//            self.initializeAppViewState()
+            SalesforceSDKManager.shared().launch()
+        }
+//        SalesforceSDKManager.shared().postLogoutAction = {
+//            [unowned self] in
+//            self.handleSdkManagerLogout()
+//        }
+//        SalesforceSDKManager.shared().switchUserAction = {
+//            [unowned self] (fromUser: SFUserAccount?, toUser: SFUserAccount?) -> () in
+//            self.handleUserSwitch(fromUser, toUser: toUser)
+//        }
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -41,6 +83,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    //MARK:  private methods
 
 }
 
---
+++ Raspberry-pi/Theromstat Control/Theromstat Control/Off.swift
@@ -0,0 +1,13 @@
+//
+//  Off.swift
+//  Theromstat Control
+//
+//  Created by moose on 5/8/17.
+//  Copyright © 2017 Fred Dunaway. All rights reserved.
+//
+
+import UIKit
+
+class Off: On {
+    
+}
